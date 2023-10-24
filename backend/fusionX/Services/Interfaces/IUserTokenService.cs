da386029a2f8afd29d25498bcc43c27850092d72
raphael-teodoro <raphaelcteodoro@hotmail.com>
2023-10-24 15:11:21 -0300

    fix: UserToken.

--- backend/fusionX/Services/Interfaces/IUserTokenService.cs
+++ backend/fusionX/Services/Interfaces/IUserTokenService.cs
@@ -5,7 +5,6 @@ namespace hackweek_backend.Services.Interfaces
     public interface IUserTokenService
     {
         Task<UserTokenDto> GenerateUserToken(string email);
-        Task<UserTokenDto?> GetUserToken(string token);
         Task DeleteUserToken(uint idUser);
     }
 }
