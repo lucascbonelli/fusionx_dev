b96df9b2f3db945527b856f4818c39b371d28e01
itscloudia <itisclaudia1@gmail.com>
2023-10-24 18:06:20 -0300

    feat: Adiciona as classes de DTO, Service e Controller para Attendance

---
+++ backend/fusionX/Controllers/AttendancesController.cs
@@ -0,0 +1,105 @@
+﻿using hackweek_backend.dtos;
+using hackweek_backend.Models;
+using hackweek_backend.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace hackweek_backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AttendanceController : ControllerBase
+    {
+        private readonly IAttendanceService _service;
+
+        public AttendanceController(IAttendanceService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(Roles = UserRoles.Company)]
+        public async Task<ActionResult<AttendanceDto?>> GetAttendanceById(uint id)
+        {
+            var attendance = await _service.GetAttendanceById(id);
+            if (attendance == null)
+            {
+                return NotFound("Attendance not found!");
+            }
+
+            return Ok(attendance);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = UserRoles.Company)]
+        public async Task<ActionResult> CreateAttendance(AttendanceDtoInsert request)
+        {
+            try
+            {
+                await _service.CreateAttendance(request);
+                return Ok("Attendance created successfully!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = UserRoles.Company)]
+        public async Task<ActionResult> UpdateAttendance(uint id, AttendanceDtoUpdate request)
+        {
+            try
+            {
+                await _service.UpdateAttendance(id, request);
+                return Ok("Attendance updated successfully!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = UserRoles.Company)]
+        public async Task<ActionResult> DeleteAttendance(uint id)
+        {
+            try
+            {
+                await _service.DeleteAttendance(id);
+                return Ok("Attendance deleted successfully!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("user/{userId}")]
+        [Authorize(Roles = UserRoles.Company)]
+        public async Task<ActionResult<IEnumerable<AttendanceDto>>> GetAttendancesByUserId(uint userId)
+        {
+            var attendances = await _service.GetAttendancesByUserId(userId);
+            return Ok(attendances);
+        }
+
+        [HttpGet("eventday/{eventDayId}")]
+        [Authorize(Roles = UserRoles.Company)]
+        public async Task<ActionResult<IEnumerable<AttendanceDto>>> GetAttendancesByEventDayId(uint eventDayId)
+        {
+            var attendances = await _service.GetAttendancesByEventDayId(eventDayId);
+            return Ok(attendances);
+        }
+
+        [HttpGet("totalconfirmed/{eventId}")]
+        [Authorize(Roles = UserRoles.Company)]
+        public async Task<ActionResult<int>> GetTotalConfirmedAttendances(uint eventId)
+        {
+            var totalConfirmedAttendances = await _service.GetTotalConfirmedAttendances(eventId);
+            return Ok(totalConfirmedAttendances);
+        }
+    }
+}
---
+++ backend/fusionX/dtos/AttendanceDtoInsert.cs
@@ -0,0 +1,9 @@
+﻿namespace hackweek_backend.dtos
+{
+    public class AttendanceDtoInsert
+    {
+        public string Status { get; set; } = string.Empty;
+        public uint LectureId { get; set; }
+        public uint UserId { get; set; }
+    }
+}
