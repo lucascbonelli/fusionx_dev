75f4b2760bae2be2bbc2b3e7533a922ed8a9fac0
ghamann <90395594+ghamann@users.noreply.github.com>
2023-10-25 15:00:12 -0300

    feat: LocationsController add GET e Delete

---
+++ fusionX/Controllers/LocationsController.cs
@@ -0,0 +1,61 @@
+﻿using hackweek_backend.dtos;
+using hackweek_backend.Models;
+using hackweek_backend.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hackweek_backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly ILocationService _service;
+
+        public LocationsController(ILocationService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("id")]
+        //[Authorize(Roles = UserRoles.Company)]
+        public async Task<ActionResult<LocationDto?>> GetLocationById(uint id)
+        {
+            var location = await _service.GetLocationById(id);
+            if (location == null)
+            {
+                return NotFound("Localização não encontrada!");
+            }
+
+            return Ok(location);
+        }
+
+        //post
+
+        //put
+
+        [HttpDelete("{id}")]
+        //[Authorize(Roles = UserRoles.Company)]
+        public async Task<ActionResult> DeleteLocation(uint id)
+        {
+            try
+            {
+                await _service.DeleteLocation(id);
+                return Ok("Localização removida com sucesso!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        //userid
+
+        //location id
+
+        //confirmed id
+
+
+    }
+}
