21da46cb824141b239fdc99942e5b2501b287b76
ghamann <90395594+ghamann@users.noreply.github.com>
2023-10-25 14:16:27 -0300

    feat: ILocationService e LocationDto

---
+++ fusionX/Services/Interfaces/ILocationService.cs
@@ -0,0 +1,16 @@
+﻿using hackweek_backend.dtos;
+
+namespace hackweek_backend.Services.Interfaces
+{
+    public interface ILocationService
+    {
+        Task<LocationDto?> GetLocationById(uint id);
+        //Task CreateLocation(LocationDtoInsert request);
+        //Task UpdateLocation(uint id, LocationDtoUpdate request);
+        Task DeleteLocation(uint id);
+
+        //Task<IEnumerable<LocationDto>> GetLocationsByUserId(uint userId);
+        //Task<IEnumerable<LocationDto>> GetLocationsByEventDayId(uint eventDayId);
+        //Task<int> GetTotalConfirmedLocations(uint eventId);
+    }
+}
