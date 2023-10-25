1041e5ec76e40b55ff0cb01a580970dd2933702c
Paulo Lisboa <paulolisboa38@gmail.com>
2023-10-25 04:33:46 -0300

    feat(dtos): Implement EventDtoCreate and EventDtoUpdate classes

---
+++ fusionX/dtos/EventDtoCreate.cs
@@ -0,0 +1,19 @@
+﻿using hackweek_backend.Models;
+
+namespace hackweek_backend.Dtos
+{
+    public class EventDtoCreate
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public byte[]? BannerImage { get; set; }
+
+        public uint UserId { get; set; }
+
+        public ICollection<EventTag>? Tags { get; set; } = new List<EventTag>();
+        public ICollection<Session>? Sessions { get; set; } = new List<Session>();
+        public ICollection<EventImage>? EventImages { get; set; } = new List<EventImage>();
+    }
+}
---
+++ fusionX/dtos/EventDtoUpdate.cs
@@ -0,0 +1,14 @@
+﻿namespace hackweek_backend.Dtos
+{
+    public class EventDtoUpdate
+    {
+        public uint Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public byte[]? BannerImage { get; set; }
+
+        public uint UserId { get; set; }
+    }
+}
