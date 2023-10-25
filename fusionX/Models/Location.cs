68028efe0a088ee8c3a0573a0668bd7e539327fd
ghamann <90395594+ghamann@users.noreply.github.com>
2023-10-24 21:38:09 -0300

    Repository only backend

--- fusionX/Models/Location.cs
+++ fusionX/Models/Location.cs
@@ -8,6 +8,7 @@
         public string City { get; set; } = string.Empty;
         public string Disctrict { get; set; } = string.Empty;
         public string Street { get; set; } = string.Empty;
+
         public int Number { get; set; }
         public string Complement { get; set; } = string.Empty;
     }
