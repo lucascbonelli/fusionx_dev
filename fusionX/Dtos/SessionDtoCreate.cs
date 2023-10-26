b8d0faf864b8241683e1e5ef8d9e7d21344d209d
Paulo Lisboa <paulolisboa38@gmail.com>
2023-10-26 01:53:51 -0300

    feat: Add DTOs for Lecture and Session creation

---
+++ fusionX/Dtos/SessionDtoCreate.cs
@@ -0,0 +1,11 @@
+﻿using EvenTech.Models;
+
+namespace EvenTech.Dtos
+{
+    public class SessionDtoCreate
+    {
+        public int Capacity { get; set; }
+        public uint? LocationId { get; set; }
+        public ICollection<LectureDtoCreate>? Lectures { get; set; }
+    }
+}
