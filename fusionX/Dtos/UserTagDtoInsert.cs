28aa7fc9830c8ef8c75bec60f18cb0709a4692e1
ghamann <90395594+ghamann@users.noreply.github.com>
2023-10-26 11:09:14 -0300

    feat: UserTag DTO's e Service

---
+++ fusionX/Dtos/UserTagDtoInsert.cs
@@ -0,0 +1,9 @@
+﻿namespace EvenTech.Dtos
+{
+    public class UserTagDtoInsert
+    {
+        public uint TagId { get; set; }
+        public uint UserId { get; set; }
+        public int Occurrences { get; set; }
+    }
+}
---
+++ fusionX/Services/Interfaces/IUserTagService.cs
@@ -0,0 +1,16 @@
+﻿using EvenTech.Dtos;
+
+namespace EvenTech.Services.Interfaces
+{
+    public interface IUserTagService
+    {
+        // Métodos CRUD
+        Task<UserTagDto?> GetUserTag(uint tagId, uint userId);
+        Task CreateUserTag(UserTagDtoInsert request);
+        Task DeleteUserTag(uint tagId, uint userId);
+
+        // Métodos Adicionais
+        Task<IEnumerable<UserTagDto>> GetUserTagsByUserId(uint userId);
+        Task<IEnumerable<UserTagDto>> GetUserTagsByTagId(uint tagId);
+    }
+}
