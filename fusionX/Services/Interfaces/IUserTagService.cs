11104074ba7fe7928cf7151abce1deb36b73f050
ghamann <90395594+ghamann@users.noreply.github.com>
2023-10-26 14:51:50 -0300

    feat: Finalizado o UserTag completo

--- fusionX/Services/Interfaces/IUserTagService.cs
+++ fusionX/Services/Interfaces/IUserTagService.cs
@@ -4,12 +4,10 @@ namespace EvenTech.Services.Interfaces
 {
     public interface IUserTagService
     {
-        // Métodos CRUD
         Task<UserTagDto?> GetUserTag(uint tagId, uint userId);
         Task CreateUserTag(UserTagDtoInsert request);
         Task DeleteUserTag(uint tagId, uint userId);
 
-        // Métodos Adicionais
         Task<IEnumerable<UserTagDto>> GetUserTagsByUserId(uint userId);
         Task<IEnumerable<UserTagDto>> GetUserTagsByTagId(uint tagId);
     }
