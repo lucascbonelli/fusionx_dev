7e4c6de001669b617bd8492a569a603233631f9f
Paulo Lisboa <paulolisboa38@gmail.com>
2023-10-27 12:48:59 -0300

    Refactor: Removed virtual keywords and made EventManagerId and User nullable

--- fusionX/Models/EventManger.cs
+++ fusionX/Models/EventManger.cs
@@ -5,15 +5,12 @@ namespace EvenTech.Models
 {
     public class EventManager
     {
-        [Key]
         public uint Id { get; set; }
 
-        [ForeignKey(nameof(Event))]
         public uint EventId { get; set; }
-        public virtual required Event Event { get; set; }
+        public Event? Event { get; set; }
 
-        [ForeignKey(nameof(User))]
         public uint UserId { get; set; }
-        public virtual required User User { get; set; }
+        public User? User { get; set; }
     }
 }
