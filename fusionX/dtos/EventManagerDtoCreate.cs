b6ef68fa8fbaf46ebf5fa3f780306375f05102f9
Paulo Lisboa <paulolisboa38@gmail.com>
2023-10-27 16:13:06 -0300

    feat: Add EventManagerDtoCreate to EvenTech.dtos namespace

---
+++ fusionX/dtos/EventManagerDtoCreate.cs
@@ -0,0 +1,10 @@
+﻿using EvenTech.Models;
+
+namespace EvenTech.dtos
+{
+    public class EventManagerDtoCreate
+    {
+        public uint EventId { get; set; }
+        public uint UserId { get; set; }
+    }
+}
